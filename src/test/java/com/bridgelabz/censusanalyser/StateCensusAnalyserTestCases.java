421b5eb094222f4834f6d6fb284ba8f2dfd2039c
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-06-17 14:00:49 +0530

    ADDED:To test case check the number of record matches

--- src/test/java/com/bridgelabz/censusanalyser/StateCensusAnalyserTestCases.java
+++ src/test/java/com/bridgelabz/censusanalyser/StateCensusAnalyserTestCases.java
@@ -1,5 +1,19 @@
 package com.bridgelabz.censusanalyser;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
 public class StateCensusAnalyserTestCases  {
     private static final String SAMPLE_CSV_FILE_PATH = "src/test/resources/IndiaStateCensusData.csv";
+
+    @Test
+    public void givenCSVFile_WhenFileCorrect_ThenReturnTrueData() {
+        try {
+            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+            int counter = stateCensusAnalyser.getNumberOfRecords(SAMPLE_CSV_FILE_PATH);
+            Assert.assertEquals(29, counter);
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
+        }
+    }
 }
